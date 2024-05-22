63f3a414f1001c0a7d0f3aedb9909f87022cb94f
Asif Nawaz <160779022+asifnawazawan@users.noreply.github.com>
2024-05-23 00:42:39 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+// let month="jan"
+// console.log(month=="ramadan")
+
+// let age=17
+// console.log(age< 18, "school")
+// console.log(age >=18, "university")
+
+
+// let KE=false
+// let UPS=false
+// let bulb= KE || UPS
+// console.log(bu
+
+// let bill=false
+// let KE="light on hai"
+// let UPS="UPS on hai"
+// let bulb= bill && KE || UPS
+// console.log(bulb)
+
+
+function showarma(salt=1,
+    butter=2){
+    console.log(1+salt+butter)
+    console.log(coocked)
+    return coocked
+}
+let table1=showarma(1.5, 2)
+let table2=showarma(2, 4)
